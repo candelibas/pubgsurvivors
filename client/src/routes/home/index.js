f6f52921b3cd67ead7f4eb4d9006c37006f33c3d
Can Delibas <candelibas96@gmail.com>
2017-11-12 18:04:37 +0200

    feat: add loading indicator

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -11,37 +11,60 @@ class Home extends Component {
   constructor(props) {
     super(props);
     
-    this.state = { auth: null, steamid: null, username: null, profile: null, avatar: null };
+    this.state = { 
+      loading: true,
+      auth: null, 
+      steamid: null, 
+      username: null, 
+      profile: null, 
+      avatar: null 
+    };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     document.body.className = 'home';
     // This binding is necessary to make `this` work in the callback
     this.loginWithSteam = this.loginWithSteam.bind(this);
+
+    // Check Steam authentication
     axios.get('/authcheck')
     .then((response) => {
-      this.setState({ 
-        auth: response.data.status,
-        steamid: response.data.user.steamid, 
-        username: response.data.user.username,
-        profile: response.data.user.profile,
-        avatar: response.data.user.avatar
-      });
+
+      if(response.data.status === 'authed')
+      {
+        this.setState({
+          loading: false,
+          auth: response.data.status,
+          steamid: response.data.user.steamid, 
+          username: response.data.user.username,
+          profile: response.data.user.profile,
+          avatar: response.data.user.avatar
+        });
+      }
+
+      if(response.data.status === 'not_authed') {
+        this.setState({
+          loading: false
+        });
+      }
+      
     })
     .catch(function (error) {
       console.log(error);
     });
+
   }
 
   loginWithSteam = () => {
-    window.location = "http://pubgsurvivors.com/auth";
+    window.location = "http://localhost:5000/auth";
   }
 
   render() {
-
+    
+    const { loading, username } = this.state;  
     const user_check = this.state.username ? 
     <h3>Welcome, {this.state.username} </h3>
-    :
+    : 
     <div className="App-intro">
     <button className="button dark" onClick={this.loginWithSteam}>
       <FA name="steam" className="btn-icon" />
@@ -61,8 +84,14 @@ class Home extends Component {
         </div>
 
         <div className="App-intro">
-          
-          {user_check}
+
+          {loading &&
+            <div style={{textAlign: 'center', marginLeft: 820 + 'px'}}><Spinner name="pacman" /></div>
+          }
+
+          {!loading &&
+            user_check
+          }
 
         </div>
 
