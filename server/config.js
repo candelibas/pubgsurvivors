d3db56f0a72c4e00b5a36d14f7ba4dcd44222a76
Can Delibas <candelibas96@gmail.com>
2017-11-29 03:29:33 +0200

    chore: ssshh...top secret!

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
-export default {
+module.exports = {
   JWT_SECRET : "dafuqsecret",
   STEAM_BASE_URL: "http://api.steampowered.com",
-  STEAM_API_KEY: "9F244C44270D8C67BC15EA8ABF706FC6"
-}
+  STEAM_API_KEY: "9F244C44270D8C67BC15EA8ABF706FC6",
+  MONGO_USER: "mesnevici",
+  MONGO_PASS: "m3sn3t61"
+};
