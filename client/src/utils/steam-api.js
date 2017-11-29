27d61e85aff0eab10db46af24d2dafd4f4783cf6
Can Delibas <candelibas96@gmail.com>
2017-11-29 03:28:51 +0200

    feat: add util for steam api

--- client/src/utils/steam-api.js
+++ client/src/utils/steam-api.js
@@ -3,79 +3,35 @@ const axios = require('axios');
 const BASE_URL = 'http://api.steampowered.com';
 const STEAM_API_KEY = '9F244C44270D8C67BC15EA8ABF706FC6';
 
-export { getRepos, getUserData, isSteamLoggedIn };
+export { getUserData, isSteamLoggedIn, getSteamFriendList };
 
 function isSteamLoggedIn() {
-
+  let state = null;
   // Check Steam authentication
   axios.get('/authcheck')
-  .then((response) => {
-
-    // Set state for authenticated user
-    if(response.data.status === 'authed')
-    {
-      this.setState({
-        loading: false,
-        auth: response.data.status,
-        steamid: response.data.user.steamid, 
-        username: response.data.user.username,
-        profile: response.data.user.profile,
-        avatar: response.data.user.avatar
-      });
-    }
-    // Set loader indicator state for not authenticated user
-    if(response.data.status === 'not_authed') {
-      this.setState({
-        loading: false
-      });
-    }
-    
+  .then((res) => {
+    state = res.data.status;
   })
   .catch(function (error) {
     console.log(error);
   });
+  return state;
+} 
+
+// Get all Steam friends
+function getSteamFriendList(steam_id) {
+
+  if(steam_id) {
+    return axios.get(`
+    ${BASE_URL}/ISteamUser/GetFriendList/v0001/?key=${STEAM_API_KEY}&steamid=${steam_id}&relationship=friend
+    `);
+  } else {
+    return false;
+  }
+
   
 }
 
-function getSteamFriendList(user_id) {
-
-  axios.get(`${BASE_URL}/ISteamUser/GetFriendList/v0001/?key=${STEAM_API_KEY}&steamid=${user_id}&relationship=friend`)
-  .then((response) => {
-
-    if(response.data.friendslist)
-    {
-      return response.data.friendslist;
-    }
-
-    if(response.data.status === 'not_authed') {
-      this.setState({
-        loading: false
-      });
-    }
-    
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
-
-  fetch(`/ISteamUser/GetFriendList/v0001/?key=9F244C44270D8C67BC15EA8ABF706FC6&steamid=76561198038689431&relationship=friend', {mode: 'no-cors'})
-  .then((response) => {
-    return response.text();
-  }).then(function(text) {  
-    var obj = JSON.parse(text);
-    console.log(obj);
-  })  
-  .catch(function(error) {  
-    console.log('Request failed', error)  
-  });
-}
-
-function getRepos(username) {
-  const url = `${BASE_URL}/users/${username}/repos?per_page=250`;
-  return axios.get(url).then(response => response.data);
-}
 
 function getUserData(username) {
   return axios.all([
