ea1f700f4c1a32702fda14814139fece3278ac8b
Can Delibas <candelibas96@gmail.com>
2017-11-30 05:10:43 +0200

    refactor: make api much more relaible

--- client/src/utils/steam-api.js
+++ client/src/utils/steam-api.js
@@ -1,26 +1,16 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const BASE_URL = 'http://api.steampowered.com';
 const STEAM_API_KEY = '9F244C44270D8C67BC15EA8ABF706FC6';
 
-export { getUserData, isSteamLoggedIn, getSteamFriendList };
+export { isSteamLoggedIn, getSteamFriendList };
 
 function isSteamLoggedIn() {
-  let state = null;
-  // Check Steam authentication
-  axios.get('/authcheck')
-  .then((res) => {
-    state = res.data.status;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-  return state;
+  return axios.get('/authcheck');
 } 
 
 // Get all Steam friends
 function getSteamFriendList(steam_id) {
-
   if(steam_id) {
     return axios.get(`
     ${BASE_URL}/ISteamUser/GetFriendList/v0001/?key=${STEAM_API_KEY}&steamid=${steam_id}&relationship=friend
@@ -28,18 +18,4 @@ function getSteamFriendList(steam_id) {
   } else {
     return false;
   }
-
-  
-}
-
-
-function getUserData(username) {
-  return axios.all([
-    axios.get(`${BASE_URL}/users/${username}`),
-    axios.get(`${BASE_URL}/users/${username}/orgs`),
-  ])
-  .then(([user, orgs]) => ({
-    user: user.data,
-    orgs: orgs.data,
-  }));
 }
