da4a2a735d96d9a92522c0a8a9980943b15a3e2a
Can Delibas <candelibas96@gmail.com>
2017-12-18 20:32:07 +0200

    chore: change api

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   JWT_SECRET : "dafuqsecret",
   STEAM_BASE_URL: "http://api.steampowered.com",
-  STEAM_API_KEY: "9F244C44270D8C67BC15EA8ABF706FC6",
+  STEAM_API_KEY: "3D432119C1C5F14B3ED97FE2057DA15A",
   MONGO_USER: "mesnevici",
   MONGO_PASS: "m3sn3t61"
 };
